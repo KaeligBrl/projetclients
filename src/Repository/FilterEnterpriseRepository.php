3a7723dc247b270dc4b498ff29d87eeb32a9489c
KaeligShebam <kaelig@shebam.fr>
2024-04-10 16:27:21 +0200

    Change

--- src/Repository/FilterEnterpriseRepository.php
+++ src/Repository/FilterEnterpriseRepository.php
@@ -4,18 +4,17 @@ namespace App\Repository;
 
 use Doctrine\ORM\ORMException;
 use App\Entity\FilterEnterprise;
-use App\Entity\FilterEnterpriseType;
 use Doctrine\ORM\OptimisticLockException;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
- * @extends ServiceEntityRepository<FilterEnterpriseType>
+ * @extends ServiceEntityRepository<FilterEnterprise>
  *
- * @method FilterEnterpriseType|null find($id, $lockMode = null, $lockVersion = null)
- * @method FilterEnterpriseType|null findOneBy(array $criteria, array $orderBy = null)
- * @method FilterEnterpriseType[]    findAll()
- * @method FilterEnterpriseType[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method FilterEnterprise|null find($id, $lockMode = null, $lockVersion = null)
+ * @method FilterEnterprise|null findOneBy(array $criteria, array $orderBy = null)
+ * @method FilterEnterprise[]    findAll()
+ * @method FilterEnterprise[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class FilterEnterpriseRepository extends ServiceEntityRepository
 {
