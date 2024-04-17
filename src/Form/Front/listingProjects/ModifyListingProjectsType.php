60788d69af79fbe09135e380afac5239dc3b6499
KaeligShebam <kaelig@shebam.fr>
2024-04-17 15:40:32 +0200

    Add orber by ASC

--- src/Form/Front/listingProjects/ModifyListingProjectsType.php
+++ src/Form/Front/listingProjects/ModifyListingProjectsType.php
@@ -39,7 +39,11 @@ class ModifyListingProjectsType extends AbstractType
                 'expanded' => true,
                 'multiple' => true,
                 'label_attr' => ['class' => 'color-white'],
-                ))
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('c')
+                        ->orderBy('c.nameActivities', 'ASC'); // Remplacez 'nom' par le nom de l'attribut que vous voulez trier
+                },
+            ))
             ->add('nameEnterpriseType', EntityType::class, array(
                 'required' => true,
                 'label' => false,
@@ -48,6 +52,10 @@ class ModifyListingProjectsType extends AbstractType
                 'expanded' => true,
                 'multiple' => true,
                 'label_attr' => ['class' => 'color-white'],
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('c')
+                        ->orderBy('c.nameEnterpriseType', 'ASC'); // Remplacez 'nom' par le nom de l'attribut que vous voulez trier
+                },
             ))
 
             ->add('nameWebsites', EntityType::class, array(
@@ -58,6 +66,10 @@ class ModifyListingProjectsType extends AbstractType
                 'expanded' => true,
                 'multiple' => true,
                 'label_attr' => ['class' => 'color-white'],
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('c')
+                        ->orderBy('c.nameWebsites', 'ASC'); // Remplacez 'nom' par le nom de l'attribut que vous voulez trier
+                },
             ))
 
             ->add('submit', SubmitType::class, [
